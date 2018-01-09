734bd8f344cd83c86d572d2be6ddae5cf175f93f
Bjorn <bjossijohnson@gmail.com>
2018-01-09 15:16:10 +0100

    backend completed

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 import mongoose from "mongoose"
 import express from "express"
+import cors from "cors"
+import bodyParser from "body-parser"
 
 const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(cors())
 
 mongoose.connect("mongodb://localhost/technigo-forum", { useMongoClient: true })
 
