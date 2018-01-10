4ed9562f4dbd4382dbb193723765b49cdd3b1e66
Bjorn <bjossijohnson@gmail.com>
2018-01-10 11:42:09 +0100

    server.js

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,24 @@ mongoose.connection.once("open", () => {
 mongoose.connection.on("error", err => {
   console.error("connection error:", err)
 })
+const topic = mongoose.model("topic", {
+  //attributes for questions here.
+  name: String,
+  email: String,
+  headline: String,
+  content: String,
+  category: String,
+  date: Date,
+  isAnswered: Boolean
+})
+
+const comment = mongoose.model("comment", {
+  name: String,
+  email: String,
+  content: String,
+  date: Date,
+  isAdmin: Boolean
+})
 
 app.listen(8080, () => {
   console.log("Server running on port 8080")
