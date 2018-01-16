694bb8a4e3944f6881c7ede5db9b81cbb99eb706
Cecilia Wong <cissi.wong@gmail.com>
2018-01-16 16:38:50 +0100

    added fetch to Topic-Form

--- frontend/src/components/topic-form/index.js
+++ frontend/src/components/topic-form/index.js
@@ -6,10 +6,10 @@ export default class TopicForm extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      name: "",
-      email: "",
-      headline: "",
-      content: ""
+      name: [],
+      email: [],
+      headline: [],
+      content: []
       // category: String,
       // date: Date,
       // isAnswered: Boolean,
@@ -21,45 +21,96 @@ export default class TopicForm extends React.Component {
   //     name: event.target.name,
   //   })
   // }
+  //
+  // handleInputChange = event => {
+  //   const target = event.target
+  //   const value = target.value
+  //   const name = target.name
+  //
+  //   this.setState({
+  //     [name]: value
+  //   })
+  // }
+  handleSubmit = event => {
+    event.preventDefault()
+    fetch("http://localhost:8080/faq", {
+      method: "POST",
+      header: {
+        Accept: "application/json, textplain, */*",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(this.state)
+    }).then(response => {
+      return response.json()
+    })
+    this.setState({
+      name: "",
+      email: "",
+      headline: "",
+      content: ""
+    })
+  }
 
-//   handleInputChange = event => {
-//   const target = event.target
-//   const value = target.value
-//   const name = target.name
-//
-//   this.setState({
-//     [name]: value
-//   })
-// }
+  handleName = event => {
+    this.setState({
+      name: event.target.value
+    })
+  }
+
+  handleEmail = event => {
+    this.setState({
+      email: event.target.value
+    })
+  }
+
+  handleHeadline = event => {
+    this.setState({
+      headline: event.target.value
+    })
+  }
+
+  handleContent = event => {
+    this.setState({
+      content: event.target.value
+    })
+  }
 
   render() {
     return (
       <div>
-        <form className="topic-form" onSubmit={this.handleSubmit}>
+        <form
+          className="topic-form"
+          onSubmit={this.handleSubmit}>
           <label>
             <h3>Name:</h3>
-            <input type="text" value={this.state.name} onChange={this.handleInputChange} />
+            <input
+              name="name"
+              type="text"
+              value={this.state.name}
+              onChange={this.handleName} />
           </label>
           <label>
             <h3>Email:</h3>
             <input
+              name="email"
               type="email"
               value={this.state.email}
-              onChange={this.handleInputChange} />
+              onChange={this.handleEmail} />
           </label>
           <label>
             <h3>Topic:</h3>
             <input
+              name="headline"
               type="text"
               value={this.state.headline}
-              onChange={this.handleInputChange} />
+              onChange={this.handleHeadline} />
           </label>
           <label>
             <h3>Message:</h3>
             <textarea
+              name="content"
               value={this.state.content}
-              onChange={this.handleInputChange}
-            />
+              onChange={this.handleContent} />
           </label>
           <button
             type="submit">Submit
