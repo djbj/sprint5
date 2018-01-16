c03d3a05e338744b70ea4b18ed67cda6ff5bdcaa
Bjorn <bjossijohnson@gmail.com>
2018-01-16 15:04:54 +0100

    topic form

--- frontend/src/components/topic-form/index.js
+++ frontend/src/components/topic-form/index.js
@@ -5,7 +5,27 @@ import "style.css"
 export default class TopicForm extends React.Component {
   render() {
     return (
-      <div />
+      <div>
+        <form className="topic-form">
+          <label>
+            <h3>Name:</h3>
+            <input type="text" />
+          </label>
+          <label>
+            <h3>Email:</h3>
+            <input type="email" />
+          </label>
+          <label>
+            <h3>Topic:</h3>
+            <input type="text" />
+          </label>
+          <label>
+            <h3>Message:</h3>
+            <textarea />
+          </label>
+          <button type="submit"></button>
+        </form>
+      </div>
     )
   }
 }
--- frontend/src/components/topic/index.js
+++ frontend/src/components/topic/index.js
@@ -3,6 +3,7 @@ import uuid from "uuid/v4"
 import "style.css"
 
 export default class Topic extends React.Component {
+
   render() {
     return (
       <div />
