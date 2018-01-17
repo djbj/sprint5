da32a42a63ae02e5157f28d9a8b3878031196a97
AnnikaEkstrom <ekstromannika@hotmail.com>
2018-01-17 15:10:56 +0100

    Added routing

--- frontend/src/components/admin-view/index.js
+++ frontend/src/components/admin-view/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import uuid from "uuid/v4"
 import "style.css"
+import { BrowserRouter, Route } from "react-router-dom"
 
 export default class AdminView extends React.Component {
   render() {
--- frontend/src/components/home-view/index.js
+++ frontend/src/components/home-view/index.js
@@ -1,15 +1,21 @@
 import React from "react"
 import uuid from "uuid/v4"
 import "style.css"
+import AdminView from "../admin-view"
+import { BrowserRouter, Route } from "react-router-dom"
 
 import Search from "./search"
 
 export default class HomeView extends React.Component {
   render() {
     return (
-      <div>
-        <Search />
-      </div>
+      <BrowserRouter>
+        <div>
+          <Route exact path="/" component={HomeView} />
+          <Route path="/admin" component={AdminView} />
+        </div>
+      </BrowserRouter>
+
     )
   }
 }
