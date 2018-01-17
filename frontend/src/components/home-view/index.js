4d9aee740a16e309ce4c4c9ad1294364577628a6
Cecilia Wong <cissi.wong@gmail.com>
2018-01-17 18:13:54 +0100

    minor cleanup

--- frontend/src/components/home-view/index.js
+++ frontend/src/components/home-view/index.js
@@ -1,8 +1,5 @@
 import React from "react"
-import uuid from "uuid/v4"
 import "./style.css"
-import { BrowserRouter, Route } from "react-router-dom"
-import AdminView from "../admin-view"
 import Navigation from "../navigation"
 import TopicForm from "../topic-form"
 import TopicList from "../topic-list"
