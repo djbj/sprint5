737ba8741bd1bb68c978da44ea604ad899b2937d
Sofia Boström <sofia.bostrom@aftonbladet.se>
2018-01-17 10:35:42 +0100

    Set up Topic and Topic-list

---
+++ frontend/src/components/topic-list/index.js
@@ -0,0 +1,40 @@
+import React from "react"
+import uuid from "uuid/v4"
+import "style.css"
+import Topic from "./topic"
+
+export default class TopicList extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      TopicList: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:8080/faq").then(response => {
+      return response.json()
+    }).then(json => {
+      this.setState({ TopicList: json })
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <section className="topic-list">
+          {this.state.TopicList.map(topic => {
+            return <Topic
+              headline={topic.headline}
+              content={topic.content}
+              id={topic._id}
+              category={topic.category}
+              date={topic.date}
+              isAnswered={topic.isAnswered}
+              isVisible={topic.isVisible} />
+          })}
+        </section>
+      </div>
+    )
+  }
+}
--- frontend/src/components/topic/index.js
+++ frontend/src/components/topic/index.js
@@ -1,12 +1,10 @@
 import React from "react"
-import uuid from "uuid/v4"
-import "style.css"
+import "./style.css"
 
 export default class Topic extends React.Component {
-
   render() {
-    return (
-      <div />
+    return(
+      <div/>
     )
   }
 }
