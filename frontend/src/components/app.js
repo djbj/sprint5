f49e8168c9d4c7107cba0b62625779fc19e06385
Sofia Boström <sofia.bostrom@aftonbladet.se>
2018-01-17 12:45:03 +0100

    Moved fetch to App

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -2,18 +2,31 @@ import React from "react"
 import TopicForm from "./topic-form"
 import TopicList from "./topic-list"
 
-class App extends React.Component {
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      topicList: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:8080/faq").then(response => {
+      return response.json()
+    }).then(json => {
+      this.setState({ topicList: json })
+    })
+  }
 
   render() {
     return (
       <div>
         Find me in src/app.js!
         <TopicForm />
-        <TopicList />
+        <TopicList
+          topicList={this.state.topicList} />
       </div>
     )
   }
 
 }
-
-export default App
--- frontend/src/components/topic-list/index.js
+++ frontend/src/components/topic-list/index.js
@@ -4,26 +4,12 @@ import "./style.css"
 import Topic from "../topic"
 
 export default class TopicList extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      TopicList: []
-    }
-  }
-
-  componentDidMount() {
-    fetch("http://localhost:8080/faq").then(response => {
-      return response.json()
-    }).then(json => {
-      this.setState({ TopicList: json })
-    })
-  }
 
   render() {
     return (
       <div>
         <section className="topic-list">
-          {this.state.TopicList.map(topic => {
+          {this.props.topicList.map(topic => {
             return <Topic
               headline={topic.headline}
               content={topic.content}
