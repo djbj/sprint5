08149cff1f3410c24c6865c47b1a2852d2fa1e3c
Sofia Boström <sofia.bostrom@aftonbladet.se>
2018-01-19 11:38:12 +0100

    Css for the comment

--- frontend/src/components/comment/index.js
+++ frontend/src/components/comment/index.js
@@ -6,10 +6,10 @@ export default class Comment extends React.Component {
   render() {
     return (
         <div className="comment">
-          <p className="name">{this.props.comment.name}</p>
-          <p className="email">{this.props.comment.email}</p>
           <p className="content">{this.props.comment.content}</p>
-          <p className="date">{this.props.comment.date}</p>
+          <p className="userName">{this.props.comment.name}</p>
+          <p className="userName">{this.props.comment.email}</p>
+          {/* <p className="date">{this.props.comment.date}</p> */}
         </div>
       )
     }
